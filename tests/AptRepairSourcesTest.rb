86de67c0a8a575cef69fe11276eaa86ec3e96f37
till <till@php.net>
2011-11-22 16:25:02 +0000

    woo, unit tests

---
+++ tests/AptRepairSourcesTest.rb
@@ -0,0 +1,23 @@
+$: << File.join(File.dirname(__FILE__), "..", "lib")
+require 'rubygems'
+require 'net/http'
+require 'AptRepairSources'
+require 'test/unit'
+
+class AptRepairSourcesTest < Test::Unit::TestCase
+  def test_archive
+    l = "deb http://archive.ubuntu.com/ubuntu/ karmic main universe"
+    e = "deb http://old-releases.ubuntu.com/ubuntu/ karmic main universe"
+
+    helper = AptRepairSources.new(l)
+    assert_equal(e, helper.fix_line)
+  end
+
+  def test_multiverse_security
+    l = "deb-src http://security.ubuntu.com/ubuntu karmic-security main universe"
+    e = "deb-src http://old-releases.ubuntu.com/ubuntu karmic-security main universe"
+
+    helper = AptRepairSources.new(l)
+    assert_equal(e, helper.fix_line)
+  end
+end
